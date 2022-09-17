b017778514772e005f4042c01d586640d4793fbf
yungshun317 <seraph317@gmail.com>
2022-09-17 21:23:35 +0800

    [CHORE] Create a Spring project using Spring Initializr

---
+++ src/main/java/yungshun/chang/springcharactergeneratorxmlconfiguration/SpringCharacterGeneratorXmlConfigurationApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springcharactergeneratorxmlconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCharacterGeneratorXmlConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCharacterGeneratorXmlConfigurationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yungshun/chang/springcharactergeneratorxmlconfiguration/SpringCharacterGeneratorXmlConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springcharactergeneratorxmlconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCharacterGeneratorXmlConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
